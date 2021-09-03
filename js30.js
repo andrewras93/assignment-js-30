fcf39593d6cfd1694ad91a6ac0edfe3e79d3986e
andrewras93 <andrewras93@gmail.com>
2021-09-03 14:54:16 +0200

    Initial commit.

---
+++ js30.js
@@ -0,0 +1,8 @@
+let a = 8 / 2 * (2 + 2);
+let b = 8 / 2 * 2 + 2;
+
+let predictionA = 16;
+let predictionB = 10;
+
+console.log(`My prediction for calculation a = ${predictionA}. Actual result = ${a}`);
+console.log(`My prediction for calculation b = ${predictionB}. Actual result = ${b}`);
